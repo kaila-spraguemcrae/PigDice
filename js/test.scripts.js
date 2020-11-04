70aa70011df416b6ca20b3d24aa0f82be73251cc
David Sterry <sterry.david@gmail.com>
2020-11-04 14:45:20 -0800

    test addRollDice prototype- pass test
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- js/test.scripts.js
+++ js/test.scripts.js
@@ -1,6 +1,7 @@
-let roll = function rollDice() {
-  return = 1 + Math.floor(Math.random() * 6);
+function rollDice() {
+  return 1 + Math.floor(Math.random() * 6);
 }
+let roll = rollDice();
 
 function UserInfo(name) {
   this.name = name;
@@ -9,12 +10,16 @@ function UserInfo(name) {
   this.total = 0;
 }
 
-UserInfo.prototype.addRollDice = function(roll) {
-  if (this rollEvent === 1) {
+UserInfo.prototype.rollDice = function(){
+  this.rollEvent = roll;
+}
+
+UserInfo.prototype.addRollDice = function() {
+  if (roll === 1) {
   this.turntotal = 0;
  //alert (your turn is over)
   } else {
-    this.turntotal += this.rollEvent;
+    this.turntotal += roll;
   }
 }
 
@@ -23,7 +28,55 @@ let playerOne = new UserInfo ("player one")
 
 playerOne.addRollDice();
 
-playerOne.roll = rollDice();
-
 console.log(roll);
-console.log(playerOne.turntotal);
+
+console.log(playerOne);
+
+//
+
+function rollDice() {
+  return 1 + Math.floor(Math.random() * 6);
+}
+
+let roll = rollDice();
+
+function UserInfo(name) {
+  this.name = name;
+  //this.rollEvent = 0;
+  this.turntotal = 0;
+  this.total = 0;
+}
+
+/* UserInfo.prototype.rollDice = function(){
+  this.rollEvent = roll;
+}
+ */
+UserInfo.prototype.addRollDice = function() {
+	
+  if (roll === 1) {
+    this.turntotal = 0;
+ //alert (your turn is over)
+  } else if (roll > 1) {
+    this.turntotal += roll;
+  }
+  
+}
+
+let playerOne = new UserInfo ("player one")
+//let playerTwo = new UserInfo ("player two")
+
+/* playerOne.addRollDice(); */
+playerOne.addRollDice();
+console.log("Dice Roll: " + roll);
+console.log(playerOne);
+playerOne.addRollDice();
+console.log("Dice Roll: " + roll);
+console.log(playerOne);
+playerOne.addRollDice();
+console.log("Dice Roll: " + roll);
+console.log(playerOne);
+
+
+/* console.log(playerOne.addRollDice()); */
+
+console.log(playerOne);
